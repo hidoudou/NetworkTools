7a100fd18e72730c579e7dd2cd0fcfbd8536d5cd
doudou <hidoudouhi@163.com>
2018-05-23 16:22:48 +0800

    add send broadcast function
    1,add send button
    2,surport send and receive at the same time

--- UdpDevSearch/app/src/main/cpp/native-lib.cpp
+++ UdpDevSearch/app/src/main/cpp/native-lib.cpp
@@ -8,6 +8,6 @@ JNICALL
 Java_com_doudou_lc_devsearch_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "            Talk is cheap, \nShow me the fucking code🤘";
     return env->NewStringUTF(hello.c_str());
 }
--- UdpDevSearch/app/src/main/java/com/doudou/lc/devsearch/MainActivity.java
+++ UdpDevSearch/app/src/main/java/com/doudou/lc/devsearch/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
     private String mStrToSend;
 
-    private boolean mStopSend = false;
+    private boolean mStopSend = true;
 
     private boolean mStopReceive = false;
 
@@ -62,17 +62,52 @@ public class MainActivity extends AppCompatActivity {
 
         initData();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab_send = (FloatingActionButton) findViewById(R.id.fab_send);
+        fab_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Snackbar.make(view, "start sending broadcast,lalalalalala~", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+                mStopSend = false;
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            while(!mStopSend) {
+                                Log.d("receive", "begin sending awesome broadcast ");
+                                //准备待发送数据
+                                byte[] bytesToSend = new byte[32];
+                                bytesToSend[0] = (byte)0xA3;
+                                bytesToSend[1] = 1;
+                                bytesToSend[16] = 2;
+                                try {
+                                    final DatagramPacket packetToSend = new DatagramPacket(bytesToSend, bytesToSend.length, InetAddress.getByName("255.255.255.255"), BROADCAST_PORT);
+
+                                    mSendSocket.send(packetToSend);
+                                }catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+
+                                Thread.sleep(1000);
+                            }
+                        }catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_recv);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "starting receving broadcast balabalabala~", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-
-                //开始收包
-                byte[] result = new byte[1024];
-                final DatagramPacket receiveBuffer = new DatagramPacket(result, result.length);
 
                 new Thread(new Runnable() {
                     @Override
@@ -80,13 +115,31 @@ public class MainActivity extends AppCompatActivity {
                         try {
                             while(!mStopReceive) {
                                 Log.d("receive", "begin receive awesome broadcast ");
+                                //开始收包
+                                byte[] result = new byte[1024];
+                                final DatagramPacket receiveBuffer = new DatagramPacket(result, result.length);
+                                receiveBuffer.setData(result);
                                 mReceiveSocket.receive(receiveBuffer);
 
-                                byte[] bytes = receiveBuffer.getData();
-                                if((byte)0xA3 == bytes[0]) {
-                                    Log.d("receive", "received awesome a3!!!");
+                                if(receiveBuffer.getLength() > 0) {
+                                    String src = receiveBuffer.getAddress().getHostAddress() + ":" + receiveBuffer.getPort();
+
+                                    byte[] bytes = receiveBuffer.getData();
+                                    byte oriHdr = bytes[0];
+                                    /*
+                                    这里将第一个字节以二进制形式打印出来，在windows机器上测试，不会显示很多位，但是在mac上测试却显示32位，
+                                    比如如果第一个字节是0xa3，转换为二进制串后是：
+                                    11111111111111111111111110100011
+                                    ，注意高位全部是1，怀疑和不同平台的默认填充方式有关系，待进一步验证。
+                                     */
+                                    String hdr = Integer.toBinaryString(oriHdr);
+                                    Log.d("receive", "receive awesome " + hdr + "\n from " + src);
+
+                                    if((byte)0xA3 == bytes[0]) {
+                                        Log.d("receive", "received awesome a3");
+                                    }
+
                                 }
-                                Log.d("receive", "receive awesome " + String.valueOf(bytes[0]));
                             }
                         }catch (Exception e) {
                             e.printStackTrace();
@@ -104,12 +157,23 @@ public class MainActivity extends AppCompatActivity {
     private void initData() {
         try {
             if(null == mReceiveSocket) {
-                mReceiveSocket = new DatagramSocket(5050);
+                mReceiveSocket = new DatagramSocket(null);
 
                 mReceiveSocket.setReuseAddress(true);
-                //mReceiveSocket.bind(new InetSocketAddress(MULTICAST_PORT));
+                mReceiveSocket.bind(new InetSocketAddress(BROADCAST_PORT));
                 mReceiveSocket.setBroadcast(true);
                 mReceiveSocket.setSoTimeout(0);
+
+                Log.d("receive", "receive socket bind " + String.valueOf(mReceiveSocket.getLocalPort()));
+            }
+
+            if(null == mSendSocket) {
+                mSendSocket = new DatagramSocket(5051);
+                mSendSocket.setReuseAddress(true);
+                mSendSocket.setBroadcast(true);
+                mSendSocket.setSoTimeout(1);
+
+                Log.d("receive", "send socket bind " + String.valueOf(mSendSocket.getLocalPort()));
             }
         } catch (Exception e) {
             Log.d("receive", "create receive socket failed!");
